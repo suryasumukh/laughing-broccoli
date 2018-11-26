e510be8ca47ecbe568adfddea13517552209fa6a
Surya Sumukh <surya.sumukh@gmail.com>
2018-11-26 09:23:42 -0500

    updated run.py

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ from ratelimit import limits, sleep_and_retry
 from collections import Counter, deque
 
 from typing import Dict, Any, Tuple
-import csv
 
 import logging
 
@@ -25,7 +24,7 @@ class PLOSAPIReader(object):
     @limits(calls=300, period=60 * 60)
     @limits(calls=10, period=60)
     def __next__(self):
-        logger.debug('Fetching articles starting from {}'.format(self._start))
+        logger.debug('Fetching PLOS articles starting from {}'.format(self._start))
         response = requests.get(url=self.url.format(self._start))
         if response.status_code == 200:
             self._start += 100
@@ -45,49 +44,44 @@ class PLOSAPIReader(object):
         return self
 
 
-class TestReader(object):
-    def __init__(self, n):
-        self.n = n
+class DOAJAPIReader(object):
+    def __init__(self):
+        self.url = "https://doaj.org/api/v1/search/articles/tobacco?page=0&pageSize=100"
         self._start = 0
-        self.sample = {'id': '10.1371/journal.pone.0076005',
-                       'journal': 'PLoS ONE',
-                       'eissn': '1932-6203',
-                       'publication_date': '2013-10-01T00:00:00Z',
-                       'article_type': 'Research Article',
-                       'author_display': ['Kolappan Chockalingam',
-                                          'Chandrasekaran Vedhachalam',
-                                          'Subramani Rangasamy',
-                                          'Gomathi Sekar',
-                                          'Srividya Adinarayanan',
-                                          'Soumya Swaminathan',
-                                          'Pradeep Aravindan Menon'],
-                       'abstract': [
-                           'Background: Tobacco use leads to many health complications and is a risk factor for the occurrence of cardio vascular diseases, lung and oral cancers, chronic bronchitis etc. Almost 6 million people die from tobacco-related causes every year. This study was conducted to measure the prevalence of tobacco use in three different areas around Chennai city, south India. Methods: A survey of 7510 individuals aged >\u200a=\u200a15 years was undertaken covering Chennai city (urban), Ambattur (semi-urban) and Sriperumbudur (rural) taluk. Details on tobacco use were collected using a questionnaire adapted from both Global Youth Tobacco Survey and Global Adults Tobacco Survey. Results: The overall prevalence of tobacco use was significantly higher in the rural (23.7%) compared to semi-urban (20.9%) and urban (19.4%) areas (P value <0.001) Tobacco smoking prevalence was 14.3%, 13.9% and 12.4% in rural, semi-urban and urban areas respectively. The corresponding values for smokeless tobacco use were 9.5%, 7.0% and 7.0% respectively. Logistic regression analysis showed that the odds of using tobacco (with smoke or smokeless forms) was significantly higher among males, older individuals, alcoholics, in rural areas and slum localities. Behavioural pattern analysis of current tobacco users led to three groups (1) those who were not reached by family or friends to advice on harmful effects (2) those who were well aware of harmful effects of tobacco and even want to quit and (3) those are exposed to second hand/passive smoking at home and outside. Conclusions: Tobacco use prevalence was significantly higher in rural areas, slum dwellers, males and older age groups in this region of south India. Women used mainly smokeless tobacco. Tobacco control programmes need to develop strategies to address the different subgroups among tobacco users. Public health facilities need to expand smoking cessation counseling services as well as provide pharmacotherapy where necessary. '],
-                       'title_display': 'Prevalence of Tobacco Use in Urban, Semi Urban and Rural Areas in and around Chennai City, India',
-                       'score': 6.777194}
+        self._delay = 10
 
     @sleep_and_retry
-    @limits(calls=300, period=60 * 60)
-    @limits(calls=10, period=60)
+    @limits(calls=5, period=2)
     def __next__(self):
-        if self._start < self.n:
-            self._start += 100
-            return [self.sample] * 200
-        else:
+        logger.debug('Fetching DOAJ articles from: {}'.format(self.url))
+        if not self.url:
+            logger.info('No more articles for `tobacco` from DOAJ. Stopping iteration')
             raise StopIteration()
+
+        response = requests.get(url=self.url)
+        if response.status_code == 200:
+            content = response.json()
+            self.url = content.get('next', None)
+            articles = content['results']
+            logger.info('DOAJ docs length: {}'.format(len(articles)))
+            return articles
+        elif response.status_code == 429:
+            time.sleep(self._delay)
+        else:
+            logger.info('DOAJ returned no content. Status Code: {}'.format(response.status_code))
+            return []
 
     def __iter__(self):
         return self
 
 
 class DOAJValidator(threading.Thread):
-    def __init__(self, source: queue.Queue, sink):
+    def __init__(self, store, source: queue.Queue,
+                 sink: queue.Queue):
         super(DOAJValidator, self).__init__()
+        self._store = store
         self._source = source
         self._sink = sink
-        self._delay = 10
-
-        self._doaj_url = "https://doaj.org/api/v1/search/articles/doi:{}"
 
     def run(self):
         while True:
@@ -98,33 +92,23 @@ class DOAJValidator(threading.Thread):
             self._source.task_done()
 
     def validate_and_link(self, task: Tuple[int, Dict[str, Any]]):
-        article_id, article = task
-        doi = article.get('id', None)
+        article_id, plos_article = task
+        doi = plos_article.get('id', None)
         if doi:
-            logger.debug('Querying DOAJ for doi: {}'.format(doi))
-            response = requests.get(url=self._doaj_url.format(doi))
-            if response.status_code == 200:
-                results = response.json().get('results', None)
-                if results:
-                    doaj_article = results[0]
-                    article['doaj_id'] = doaj_article['id']
-                    article['doaj_authors'] = doaj_article['bibjson'].get('author', [])
-                    self._sink.put((article_id, article))
-                    logger.info('queued article: {} / doi: {} for further processing'.format(article_id, doi))
-                else:
-                    logger.info('DOAJ Query did not return results for doi: {}, skipping article'.format(doi))
-            elif response.status_code == 429:
-                # Throttle processing
-                logger.info('DOAJ Server returned status {}, waiting for {}s before next request'.format(
-                    response.status_code,
-                    self._delay))
-                self._source.put(task)
-                time.sleep(self._delay)
+            doaj_article = self._store.get(doi, None)
+            if not doaj_article:
+                logging.info('No DOAJ article found for article: {} / doi: {}'.format(article_id, doi))
             else:
-                logger.info('Query returned status {} for doi: {}'.format(response.status_code, doi))
-                self._source.put(task)
+                bibjson = doaj_article.get('bibjson', None)
+                if not bibjson:
+                    logging.info('No bibjson found for article: {} / doi: {}'.format(article_id, doi))
+                else:
+                    doaj_authors = bibjson.get('author', [])
+                    plos_article['doaj_authors'] = doaj_authors
+                    plos_article['doaj_id'] = doaj_article['id']
+                    self._sink.put((article_id, plos_article))
         else:
-            logger.info('No DOI found for article {}, skipping article'.format(article_id))
+            logger.info('No DOI found for article {}, skipping article.'.format(article_id))
 
 
 class Counters(threading.Thread):
@@ -138,18 +122,16 @@ class Counters(threading.Thread):
 
     def run(self):
         while True:
-            article = self._source.get()
-            if not article:
+            task = self._source.get()
+            if not task:
                 break
 
-            self.update_counters(article)
+            self.update_counters(task)
             self._source.task_done()
 
-    def update_counters(self, task):
+    def update_counters(self, task: Tuple[int, Dict[str, Any]]):
         article_id, article = task
         doaj_authors = article.get('doaj_authors', [])
-        doaj_id = article.get('doaj_id', None)
-        plos_id = article.get('id', None)
 
         authors = []
         deps = []
@@ -170,7 +152,7 @@ class Counters(threading.Thread):
 
         logger.info('Updating counts for article: {}'.format(article_id))
         self._authors.update(authors)
-        self._departments.update(departments)
+        self._departments.update(deps)
         self._journals.update(journals)
 
 
@@ -185,16 +167,32 @@ def save_csv(counter: Counter, filename: str):
                 fh.write(line)
 
 
-if __name__ == '__main__':
-    # reader = TestReader(1)
-    reader = PLOSAPIReader()
+def main():
+    # Instantiate and load DOAJ data in memory
+    doaj_store = dict()
+    doaj_iterator = DOAJAPIReader()
+    logger.debug('Starting store setup')
+
+    for articles in doaj_iterator:
+        for article in articles:
+            bibjson = article.get('bibjson', None)
+            if bibjson:
+                identifiers = bibjson.get('identifier', [])
+                for identifier in identifiers:
+                    if identifier['type'].lower() == 'doi':
+                        doi = identifier['id']
+                        doaj_store[doi] = article
+
+    logger.info('DOAJ store setup complete')
+
+    plos_iterator = PLOSAPIReader()
 
     source = queue.Queue()
     sink = queue.Queue()  # deque()
 
     nb_workers = 2
 
-    workers = [DOAJValidator(source, sink) for _ in range(nb_workers)]
+    workers = [DOAJValidator(doaj_store, source, sink) for _ in range(nb_workers)]
     for worker in workers:
         worker.start()
 
@@ -210,7 +208,7 @@ if __name__ == '__main__':
         counter.start()
 
     page = 0
-    for articles in reader:
+    for articles in plos_iterator:
         if articles:
             for idx, article in enumerate(articles):
                 source.put((page + idx, article))
@@ -238,5 +236,9 @@ if __name__ == '__main__':
     print(departments)
 
     save_csv(authors, 'authors.csv')
-    save_csv(departments, 'departments.csv')
     save_csv(journals, 'journals.csv')
+    save_csv(departments, 'departments.csv')
+
+
+if __name__ == '__main__':
+    main()
